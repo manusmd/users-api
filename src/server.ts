a62f95c80d3211a4abc43c13fbf6e97332023496
“manusmd” <manuel.schmid@js-tut.de>
2021-10-26 11:36:42 +0200

    Add name param

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/api/users/:name', (request, response) => {
+  response.send(request.params);
+});
+
+app.get('/api/users', (_request, response) => {
+  const users = [{ name: 'Leon' }, { name: 'Dennis' }, { name: 'David' }];
+  response.send(users);
+});
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
